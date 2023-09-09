5ee045feab90d356d27a31d147c8952c5189dc74
ayronman99 <itechman32@gmail.com>
2023-09-09 09:29:24 +0800

    Leenard commit

--- phasebook/match.py
+++ phasebook/match.py
@@ -13,15 +13,14 @@ def match(match_id):
         return "Invalid match id", 404
 
     start = time.time()
-    msg = "Match found" if (is_match(*MATCHES[match_id])) else "No match"
+    msg = "Match found" if (set_matcher(*MATCHES[match_id])) else "No match"
     end = time.time()
 
-    return {"message": msg, "elapsedTime": end - start}, 200
+    return {"message": msg, "searchTime": end - start}, 200
 
 
-def is_match(fave_numbers_1, fave_numbers_2):
-    for number in fave_numbers_2:
-        if number not in fave_numbers_1:
-            return False
+def set_matcher(fave_numbers_1, fave_numbers_2):
+    num_set_1 = set(fave_numbers_1)
+    num_set_2 = set(fave_numbers_2)
 
-    return True
+    return all(number in num_set_1 for number in num_set_2)
--- phasebook/search.py
+++ phasebook/search.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 
 from .data.search_data import USERS
 
@@ -12,19 +12,73 @@ def search():
 
 
 def search_users(args):
-    """Search users database
+    """
+    Search users database
 
     Parameters:
         args: a dictionary containing the following search parameters:
             id: string
             name: string
-            age: string
+            age: int
             occupation: string
 
     Returns:
         a list of users that match the search parameters
     """
+    user_list = []
 
-    # Implement search here!
+    get_user_id = query_by_id()
+    get_user_name = query_by_name()
+    get_user_age = query_by_age()
 
-    return USERS
+    user_list.extend(get_user_id)
+    user_list.extend(get_user_name)
+    # user_list.extend(get_user_age)
+
+    if len(user_list) == 0 or user_list[0] is None:
+        user_list = USERS
+
+    return user_list
+
+
+def query_by_id():
+    user_id = request.args.get("id")
+
+    if user_id:
+        query_user_id = [next((user for user in USERS if user["id"] == user_id), None)]
+        if query_user_id:
+            return query_user_id
+        else:
+            return []
+
+
+
+def query_by_name():
+    user_name = request.args.get("name")
+
+    if user_name:
+        query_user_names = [
+            user for user in USERS if user_name.lower() in user.get("name").lower()
+        ]
+        if query_user_names:
+            return query_user_names
+    else:
+        return []
+
+
+def query_by_age():
+    user_age = request.args.get("age")
+
+    if user_age:
+        query_user_age = [
+            user
+            for user in USERS
+            if user["age"] in (int(user_age), int(user_age) - 1, int(user_age) + 1)
+        ]
+        if query_user_age:
+            return query_user_age
+        else:
+            return []
+        
+
+# def query_by_occupation: unimplemented
